dd0fa9c515ceb26b3fb619f8bc469d380973500b
KoracM <mustafakorac@gmail.com>
2023-07-27 01:51:57 +0300

    Hackerrank if-else challange has been accomplished

---
+++ untitled/src/kosullu_ifadeler_ve_kod_bloklari/odevler/hackerrank_if_else/main.java
@@ -0,0 +1,21 @@
+package kosullu_ifadeler_ve_kod_bloklari.odevler.hackerrank_if_else;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n;
+        System.out.print("Insert the number: ");
+        n=input.nextInt();
+
+        if(n%2==0){
+            if((n>=2 &&n<=5)||n>20)
+                System.out.println("Not Weird");
+            else if (n>5 && n<=20)
+                System.out.println("Weird");
+        }
+        else
+            System.out.println("Weird");
+    }
+}
